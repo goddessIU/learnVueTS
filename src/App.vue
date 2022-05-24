4c57dbb81c1d66d60de0fd0507503c1bf59d95e4
goddessIU <2581606394@qq.com>
2022-05-24 23:55:23 +0800

    feat<*>: 完成了lesson2

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref<string>('Link')
+const age = ref<string | number>(25)
+
+const changeName = (val: string) => {
+  name.value = val
+}
+
+const changeAge = (val: number | string) => {
+  age.value = val
+}
+</script>
+
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('sb')">change Name</button>
+    <button @click="changeAge('30')">change Age</button>
+  </div>
+</template>
+
+<style>
+
+</style>
