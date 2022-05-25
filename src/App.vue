b2f3be0234a28126d0d0e8fc58c5295dce9d0703
goddessIU <2581606394@qq.com>
2022-05-25 11:56:01 +0800

    feat<*> 七节课程完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import type Job from './types/Job';
+import JobList from './components/JobList.vue';
+import type OrderTerm from './types/OrderTerm';
 
-const name = ref<string>('Link')
-const age = ref<string | number>(25)
+const jobs = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 3000, id: '1' },
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
 
-const changeName = (val: string) => {
-  name.value = val
-}
+])
 
-const changeAge = (val: number | string) => {
-  age.value = val
+const order = ref<OrderTerm>('title')
+
+const handleClick = (term: OrderTerm) => {
+  order.value = term
 }
 </script>
 
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('sb')">change Name</button>
-    <button @click="changeAge('30')">change Age</button>
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('location')">Order by Location</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order"/>
   </div>
+  
 </template>
 
-<style>
-
+<style scoped>
+header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
